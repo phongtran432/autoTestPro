886a3633bbb0b197435b608db6edf668b5c1a59b
phongtranone <phongtranone@gmail.com>
2017-02-16 20:07:24 +0700

    Auto for add new site

--- selenium-java/src/main/java/org/seleniumhq/selenium/selenium_java/App.java
+++ selenium-java/src/main/java/org/seleniumhq/selenium/selenium_java/App.java
@@ -1,9 +1,18 @@
 package org.seleniumhq.selenium.selenium_java;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.bcel.generic.Select;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,15 +20,47 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import junit.framework.Assert;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException
+    public static void main( String[] args ) throws InterruptedException, IOException
     {
-    	WebDriver driver;
+    	String filePath = "fileInput/ocTestData.xlsx";
+    	FileInputStream inputStream = new FileInputStream(new File(filePath));
+    	Workbook workbook = new XSSFWorkbook(inputStream);
+    	Sheet firstSheet = workbook.getSheetAt(0);
+    	Iterator<Row> iterator = firstSheet.iterator();
+    	int countRow = 0;
+    	
+    	while (iterator.hasNext()){
+    		Row nextRow = iterator.next();
+    		Iterator<Cell>cellIterator = nextRow.cellIterator();
+    		
+    		
+    		while(cellIterator.hasNext()){
+    			Cell cell = cellIterator.next();
+    			
+    			switch (cell.getCellType()) {
+				case Cell.CELL_TYPE_STRING:
+					System.out.print(cell.getStringCellValue());
+					break;
+				case Cell.CELL_TYPE_NUMERIC:
+					System.out.print(cell.getNumericCellValue());
+					break;
+				}
+    			System.out.print("||");
+    		}
+    		System.out.println("");
+    		countRow++;
+    	}
+    	
+    	System.out.print("Total row: " + countRow);
+    	/*WebDriver driver;
     	System.setProperty("webdriver.gecko.driver","driver//geckodriver.exe");
     	driver = new FirefoxDriver();
         driver.get("https://provider.liveandworkwell.com");
@@ -39,5 +80,5 @@ public class App
 
          option.click();   
     }
-        System.out.println("Debug");
+*/           	
 }}
---
+++ selenium-java/src/main/java/org/seleniumhq/selenium/selenium_java/addNewSite.java
@@ -0,0 +1,8 @@
+package org.seleniumhq.selenium.selenium_java;
+
+public class addNewSite {
+	private String url;
+	private String userName, passWord;
+	
+	
+}
